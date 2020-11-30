fce770f2a877964445fdb6b927bdf1ef3367c16d
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2020-11-30 20:22:48 -0300

    completed

---
+++ src/components/ChannelButton/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Container, HashtagIcon, InviteIcon, SettingsIcons } from './styles';
+
+export interface Props {
+    channelName: string;
+    selected?: boolean;
+}
+
+const ChannelButton: React.FC<Props> = ({
+    channelName,
+    selected
+}) => {
+    return (
+        <Container className={selected ? 'active' : ''}>
+            <div>
+                <HashtagIcon />
+                <span>{channelName}</span>
+            </div>
+
+            <div className="hover">
+                <InviteIcon />
+                <SettingsIcons />
+            </div>
+        </Container>
+    )
+}
+
+export default ChannelButton;
---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,54 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+    console.log(messagesRef);
+
+    useEffect(() => {
+        const div = messagesRef.current;
+        console.log(div);
+
+        if (div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef])
+
+    return (
+        <Container>
+            <Messages>
+                {Array.from(Array(15).keys()).map((n) => (
+                    <ChannelMessage
+                        key={n}
+                        author="Guilherme Rodz"
+                        date="21/06/2020"
+                        content="Hoje é meu aniversário"
+                    />
+                ))}
+
+                <ChannelMessage
+                    author="Diego Fernandes"
+                    date="21/06/2020"
+                    content={
+                        <>
+                            <Mention>@Guilherme Rodz</Mention>, me carrega no Lol e CS de novo
+                            por favor?
+                        </>
+                    }
+                    hasMention
+                    isBot
+                />
+            </Messages>
+
+            <InputWrapper>
+                <Input type="text" placeholder="Conversar em #chat-livre" />
+                <InputIcon />
+            </InputWrapper>
+        </Container>
+    )
+}
+
+export default ChannelData;
